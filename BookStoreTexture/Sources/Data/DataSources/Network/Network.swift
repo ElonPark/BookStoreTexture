4cadc091bfe000147f3f2c3792b5c64efc78ba5d
Elon Park <dev.sungwoon@gmail.com>
2022-01-30 22:33:07 +0900

    feat: SmartRetry 추가

--- BookStoreTexture/Sources/Data/DataSources/Network/Network.swift
+++ BookStoreTexture/Sources/Data/DataSources/Network/Network.swift
@@ -9,32 +9,46 @@ import Moya
 import RxSwift
 
 final class Network<Target: TargetType>: MoyaProvider<Target> {
+
   func request(
     _ target: Target,
+    retryCount: Int = 5,
+    initialDelay: Double = 0.5,
+    maxDelay: Double = 10.0,
     file: String = #file,
     function: String = #function,
     line: UInt = #line
   ) -> Single<Response> {
+    let request: Single<Response>
+
     #if DEBUG
       let requestString = "\(target.method.rawValue) \(target.path)"
-      let request = rx.request(target)
-        .do(onSuccess: { response in
-          let message = "SUCCESS: \(requestString) (\(response.description))"
-          Log.debug(fileName: file, line: line, funcName: function, message)
-        }, onError: { error in
-          let errorDescriptor = NetworkErrorDescriptor(error: error)
-          let errorLogMessage = errorDescriptor.errorLogMessage()
-          let message = "FAILURE: \(requestString)\n\(errorLogMessage)"
-          Log.warning(fileName: file, line: line, funcName: function, message)
-
-        }, onSubscribed: {
-          let message = "REQUEST: \(requestString)"
-          Log.debug(fileName: file, line: line, funcName: function, message)
-        })
-
-      return request
+      request = rx.request(target)
+        .do(
+          onSuccess: { response in
+            let message = "SUCCESS: \(requestString) (\(response.description))"
+            Log.debug(fileName: file, line: line, funcName: function, message)
+          },
+          onError: { error in
+            let errorDescriptor = NetworkErrorDescriptor(error: error)
+            let errorLogMessage = errorDescriptor.errorLogMessage()
+            let message = "FAILURE: \(requestString)\n\(errorLogMessage)"
+            Log.warning(fileName: file, line: line, funcName: function, message)
+          },
+          onSubscribed: {
+            let message = "REQUEST: \(requestString)"
+            Log.debug(fileName: file, line: line, funcName: function, message)
+          }
+        )
     #else
-      return rx.request(target)
+      request = rx.request(target)
     #endif
+
+    if retryCount > 0 {
+      let backoffStrategy = ExponentialBackoffDecorrelatedJitter(initialDelay: initialDelay, maxDelay: maxDelay)
+      return request.retry(retryCount, delayOption: .exponential(backoffStrategy))
+    } else {
+      return request
+    }
   }
 }
---
+++ BookStoreTexture/Sources/Data/DataSources/Network/SmartRetry/Backoff/Backoff.swift
@@ -0,0 +1,24 @@
+//
+//  Backoff.swift
+//  BookStoreTexture
+//
+//  Created by Elon on 2022/01/30.
+//
+
+import Foundation
+
+/// [Exponential Backoff And Jitter](https://aws.amazon.com/ko/blogs/architecture/exponential-backoff-and-jitter/)
+class Backoff {
+
+  let initialDelay: Double
+  let maxDelay: Double
+
+  init(initialDelay: Double, maxDelay: Double) {
+    self.initialDelay = initialDelay
+    self.maxDelay = maxDelay
+  }
+
+  func exponential(attempt: Double) -> Double {
+    return min(self.maxDelay, pow(2, attempt) * self.initialDelay)
+  }
+}
